d8a1bea0819fda0146a152378a1e8ca062190f38
Joel Castillo <joel.castillo@nyu.edu>
2021-04-02 22:52:00 -0400

    Initial commit
    Base commit to setup the CS6903 Vaccine Passport Checker application
    environment with poetry.

---
+++ src/CS6903-Vaccine-Passport-Checker/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for CS6903-Vaccine-Passport-Checker."""
+
+__authors__ = """Joel Castillo, Ho Yin Kenneth Chan, Gary Zhou"""
+__author_emails__ = """joel.castillo@nyu.edu, hyc449@nyu.edu, gz438@nyu.edu"""
+__version__ = "0.1.0"
